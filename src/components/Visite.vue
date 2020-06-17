852291310b61a8da864ffbaaca90d4371c9c8595
Mathieu33260 <mat_dumez@hotmail.fr>
2020-06-17 22:03:54 +0200

    visite

--- src/components/Visite.vue
+++ src/components/Visite.vue
@@ -1,5 +1,27 @@
 <template>
-
+    <div>
+        <b-carousel
+                id="carousel-fade"
+                style="text-shadow: 0px 0px 2px #000"
+                fade
+                img-width="1024"
+                img-height="400"
+                :interval="2000"
+        >
+            <b-carousel-slide
+                    caption="Expérience unique grâce aux lunettes"
+                    img-src="https://picsum.photos/1024/400/?image=10"
+            ></b-carousel-slide>
+            <b-carousel-slide
+                    caption="Expérience unique grâce aux lunettes"
+                    img-src="https://picsum.photos/1024/400/?image=12"
+            ></b-carousel-slide>
+            <b-carousel-slide
+                    caption="Expérience unique grâce aux lunettes"
+                    img-src="https://picsum.photos/1024/400/?image=22"
+            ></b-carousel-slide>
+        </b-carousel>
+    </div>
 </template>
 
 <script>
