5a5e93aa4656d3716d83cc2dea49d3b0c5a4d40f
Mathieu33260 <mat_dumez@hotmail.fr>
2020-06-24 17:23:02 +0200

    sonar page

--- src/components/Sonar.vue
+++ src/components/Sonar.vue
@@ -1,15 +1,22 @@
 <template>
     <div>
-        <b-row>
-            <img src="../assets/sonarqr.png" style="width: 100%"/>
-        </b-row>
+        <template v-if="countDown > 5">
+            <img src="../assets/sonar-4.gif" style="width: 100%"/>
+        </template>
 
-        <b-row style="position: relative" class="d-flex justify-content-center">
-            <b-col cols="10" style="bottom: 20px">
-                <input type="button" class="mb-5" style="background-color: #65FFF9; width: 100%; font: Bold 18px/20px Gotham; border: 0; height: 40px; padding-top: 10px" value="Se rendre au Centre">
-                <input type="button" style="background-color: #65FFF9; width: 100%; font: Bold 18px/20px Gotham; border: 0; height: 40px; padding-top: 10px" value="Acheter un billet">
-            </b-col>
-        </b-row>
+        <template v-else>
+            <b-row>
+                <img src="../assets/sonarqr.png" style="width: 100%"/>
+            </b-row>
+
+            <b-row style="position: relative" class="d-flex justify-content-center">
+                <b-col cols="10" style="bottom: 20px">
+                    <input @click="$router.push('/infos')" type="button" class="mb-5" style="background-color: #65FFF9; width: 100%; font: Bold 18px/20px Gotham; border: 0; height: 40px; padding-top: 10px" value="Se rendre au Centre">
+                    <input type="button" style="background-color: #65FFF9; width: 100%; font: Bold 18px/20px Gotham; border: 0; height: 40px; padding-top: 10px" value="Acheter un billet">
+                </b-col>
+            </b-row>
+        </template>
+
     </div>
 </template>
 
@@ -17,9 +24,30 @@
     export default {
         name: "Sonar",
 
+        data() {
+            return {
+                countDown : 10
+            }
+        },
+
         created() {
             this.$store.commit('setIsSonar', true);
         },
+
+        methods: {
+            countDownTimer() {
+                if(this.countDown > 0) {
+                    setTimeout(() => {
+                        this.countDown -= 1
+                        this.countDownTimer()
+                    }, 1000)
+                }
+            }
+        },
+
+        mounted() {
+            this.countDownTimer()
+        }
     }
 </script>
 
