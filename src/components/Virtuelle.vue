560c3a8106410f3a88e561b51fb011b2deccc6e9
Mathieu33260 <mat_dumez@hotmail.fr>
2020-06-24 15:08:00 +0200

    virtuelle

--- src/components/Virtuelle.vue
+++ src/components/Virtuelle.vue
@@ -1,6 +1,80 @@
 <template>
     <div>
-        <iframe src="https://app.lapentor.com/sphere/test-21499" frameborder="0" width="100%" height="400px" scrolling="no" allow="vr,gyroscope,accelerometer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
+        <b-row>
+
+            <b-col cols="6" class="d-flex flex-column justify-content-around p-5">
+                <div style="font: Bold 20px/22px Gotham;">
+                    Vous n’avez pas eu la chance de vous déplacer ?<br>
+                    Découvrez ou re-découvrez l’exposition en ligne !
+                </div>
+
+                <div>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </div>
+
+                <div class="mt-5 d-flex flex-column justify-content-between" style="width: 60%; height: 200px; border-left: 10px solid #65FFF9;">
+                    <input type="button" style="background-color: #65FFF9; width: 100%; font: Bold 16px Gotham; border: 0; height: 40px; padding-top: 10px" value="Découvrir la dernière exposition">
+                </div>
+            </b-col>
+
+            <b-col cols="6">
+                <img src="../assets/visitevirtuelletext.png" style="width: 60%"/>
+                <iframe src="https://app.lapentor.com/sphere/exposition-virtuelle" frameborder="0" width="100%" height="400px" scrolling="no" allow="vr,gyroscope,accelerometer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <b-col cols="12" class="d-flex align-items-end">
+                <div style="font: Bold 20px/22px Gotham;" class="ml-5">
+                    Les expositions en ligne
+                </div>
+            </b-col>
+        </b-row>
+
+        <b-row class="ml-5 mr-5 d-flex justify-content-between">
+            <b-col cols="4" class="d-flex flex-column justify-content-end p-5">
+                <img src="../assets/Legendes_urbainesph_F_Deval_34_format_a5.png" style="width: 100%; height: auto">
+                <b class="mt-3" style="color: #65FFF9">#<span style="font: Bold 20px/22px Gotham; color: #C4C4C4;">Redécouvrir</span></b>
+                <div class="mt-3" style="font: Bold 20px/22px Gotham;">
+                    Visite virtuelle de l'exposition
+                </div>
+                <div class="mt-3">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </div>
+                <input class="mt-3" type="button" style="background-color: #65FFF9; width: 50%; font: Bold 16px Gotham; border: 0; height: 40px; padding-top: 10px" value="Découvrir">
+            </b-col>
+
+            <b-col cols="4" class="d-flex flex-column justify-content-end p-5">
+                <img src="../assets/Legendes_urbainesph_F_Deval_34_format_a5.png" style="width: 100%; height: auto">
+                <b class="mt-3" style="color: #65FFF9">#<span style="font: Bold 20px/22px Gotham; color: #C4C4C4;">Redécouvrir</span></b>
+                <div class="mt-3" style="font: Bold 20px/22px Gotham;">
+                    Visite virtuelle de l'exposition
+                </div>
+                <div class="mt-3">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </div>
+                <input class="mt-3" type="button" style="background-color: #65FFF9; width: 50%; font: Bold 16px Gotham; border: 0; height: 40px; padding-top: 10px" value="Découvrir">
+            </b-col>
+
+            <b-col cols="4" class="d-flex flex-column justify-content-end p-5">
+                <img src="../assets/Legendes_urbainesph_F_Deval_34_format_a5.png" style="width: 100%; height: auto">
+                <b class="mt-3" style="color: #65FFF9">#<span style="font: Bold 20px/22px Gotham; color: #C4C4C4;">Redécouvrir</span></b>
+                <div class="mt-3" style="font: Bold 20px/22px Gotham;">
+                    Visite virtuelle de l'exposition
+                </div>
+                <div class="mt-3">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </div>
+                <input class="mt-3" type="button" style="background-color: #65FFF9; width: 50%; font: Bold 16px Gotham; border: 0; height: 40px; padding-top: 10px" value="Découvrir">
+            </b-col>
+        </b-row>
+
+        <b-row class="ml-5 mr-5 d-flex justify-content-end">
+            <b-col cols="8" style="height: 100px; border-right: 10px solid #65FFF9; margin-right: 15px">
+
+            </b-col>
+        </b-row>
     </div>
 </template>
 
